68eb36edd2b0b9efd4527cb99d905ad9b2b424d5
Les Matheson <lmatheson4@bloomberg.net>
2021-08-21 21:49:35 -0400

    Sync (auto)

---
+++ bash_history
@@ -0,0 +1,2 @@
+#1629596764
+echo "The plan here is to integrate cdpath stuff with tox-py into a single tool named 'cd++' (cdpp), and use makeself.sh to install the whole thing." #
---
+++ publish/publish-via-github-release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# publish/publish-via-github-release.sh
+
+Script=$(command readlink -f $0)
+Scriptdir=$(command dirname $Script)
+
+die() {
+    builtin echo "ERROR: $@" >&2
+    builtin exit 1
+}
+
+if [[ -z $sourceMe ]]; then
+    builtin cd ${Scriptdir}/../bin || die 100
+    if [[ $( command git status -s . | command wc -l 2>/dev/null) -gt 0 ]]; then
+        die "One or more files in $PWD need to be committed before publish"
+    fi
+    command git rev-parse HEAD > ./hashfile || die 104
+    builtin cd ${Scriptdir}/.. || die 101
+    version=$( bin/cdpp-version.sh | cut -f2)
+    [[ -z $version ]] && die 103
+
+    command mkdir -p ./tmp
+
+    command makeself.sh $PWD/bin $PWD/tmp/cdpp-setup-${version}.sh "cdpp ${version}" ./setup.sh  # [src-dir] [dest-file] [label] [setup-command]
+    [[ $? -eq 0 ]] && echo "Done: upload $PWD/tmp/cdpp-setup-${version}.sh to Github release page"
+fi
