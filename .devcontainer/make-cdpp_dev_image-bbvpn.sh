b8dfae277b94448d29ecfe0cc0fa63be7c50dcdf
Les Matheson <lmatheson4@bloomberg.net>
2022-07-01 01:41:21 +0000

    Add make-cdpp_dev_image-bbvpn.sh

--- .devcontainer/make-cdpp_dev_image-bbvpn.sh
+++ .devcontainer/make-cdpp_dev_image-bbvpn.sh
@@ -6,8 +6,23 @@ die() {
     exit 1
 }
 
+bbvpnDockerfile() {
+    cat <<-EOF
+FROM artifactory.inf.bloomberg.com/dpkg-python-development-base:3.10
 
-docker pull artifactory.inf.bloomberg.com/dpkg-python-development-base:3.10 || die 101
+ENV http_proxy=http://proxy.bloomberg.com:81 \
+    https_proxy=http://proxy.bloomberg.com:81 \
+    no_proxy=.bloomberg.com,10.0.0.0/8,100.0.0.0/8 \
+    NODE_EXTRA_CA_CERTS=/etc/pki/ca-trust/source/anchors/bloomberg_rootca_v2.crt
+
+EOF
+}
+
+bbvpnDockerfile  > bbvpn.dockerfile.tmp
+
+docker build -f bbvpn.dockerfile.tmp . || die 101
+
+rm bbvpn.dockerfile.tmp
 
 xhash=$(docker images | grep -E 'artifact.*python.*base.*3\.10' | awk '{print $3}')
 [[ -n $xhash ]] || die 102
