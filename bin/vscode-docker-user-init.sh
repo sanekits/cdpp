10637dc3e122133a7d1efd2eaf509d5569cb1222
Les Matheson <lmatheson4@bloomberg.net>
2022-07-01 01:45:24 +0000

    sync

--- bin/vscode-docker-user-init.sh
+++ bin/vscode-docker-user-init.sh
@@ -2,7 +2,13 @@
 # vscode-docker-user-init.sh
 # Invoked when vscode builds the dev container, our job is to improve the 'vscode' user environment
 
+die() {
+    echo "ERROR: $*" >&2
+    exit 1
+}
+
 Script=$(readlink -f $0)
+
 
 cat >> /home/vscode/.bashrc <<-EOF
 alias lr='ls -lirta'
@@ -17,4 +23,6 @@ PATH=\$PATH:/host_home/bin
     touch ~/.vshinit
 }
 EOF
+
+mkdir ~/bb-cert && cd ~/bb-cert && ln -sf /home/lmatheson4/bb-cert/bloomberg-root-ca.crt ./Bloomberg_LP_CORP_CLASS_1_ROOT_G2.pem || die 101
 
