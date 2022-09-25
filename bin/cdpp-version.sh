9f98df2107677d9e57c0d57583148c8655ce1403
Les Matheson <lmatheson4@bloomberg.net>
2022-09-24 23:59:47 -0400

    Bump to 0.7.6

--- bin/cdpp-version.sh
+++ bin/cdpp-version.sh
@@ -2,7 +2,7 @@
 
 # Running cdpp-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=0.7.5
+KitVersion=0.7.6
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
