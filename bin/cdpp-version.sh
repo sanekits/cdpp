230add447a07bf7e74181ee9b622e02302aa5336
Les Matheson <lmatheson4@bloomberg.net>
2022-11-11 10:52:48 -0500

    Sync (auto)

--- bin/cdpp-version.sh
+++ bin/cdpp-version.sh
@@ -2,7 +2,7 @@
 
 # Running cdpp-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=0.7.6
+KitVersion=0.8.0
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
